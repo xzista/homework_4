49055d0ac643b5600a0615fbd7a4428625c6d050
xziste <17alexdmz99@gmail.com>
2025-08-24 02:50:00 +0300

    registered Contact in admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Contact
 
 
 @admin.register(Category)
@@ -13,3 +13,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category',)
     list_filter = ('category',)
     search_fields = ('name', 'description',)
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone', 'email',)
+    search_fields = ('name', 'address', 'phone', 'email',)
