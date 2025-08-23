7841d1064bee96dd0765a2013367a0e5d7a124af
xziste <17alexdmz99@gmail.com>
2025-08-23 09:22:51 +0300

    update views.py and home.html to display 5 latest products on home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    latest_products = Product.objects.all().order_by('-created_at')[:5]
+
+    print("ПОСЛЕДНИЕ 5 ПРОДУКТОВ:")
+    for i, product in enumerate(latest_products, 1):
+        print(f'{i}. {product.name} категории: {product.category}, с ценой: {product.price} руб.')
+
+    context = {
+        'latest_products': latest_products
+    }
+
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
