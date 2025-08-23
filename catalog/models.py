b1dffe8e44d12f4de7c9d9eab07c35027dc68a04
xziste <17alexdmz99@gmail.com>
2025-08-24 02:44:39 +0300

    add model contact in models.py

--- catalog/models.py
+++ catalog/models.py
@@ -30,3 +30,18 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название компании')
+    address = models.TextField(verbose_name='Адрес',blank=True,null=True)
+    phone = models.CharField(max_length=20, verbose_name='Телефон',blank=True,null=True)
+    email = models.EmailField(verbose_name='Email',blank=True,null=True)
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
+        ordering = ['name',]
+        
+    def __str__(self):
+        return self.name
