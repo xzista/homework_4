5ec4a98694be4d90d1fb82e96c755e3172b4188c
xziste <17alexdmz99@gmail.com>
2025-08-24 03:33:36 +0300

    updated media urls and settings

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+
+from django.conf.global_settings import MEDIA_URL, MEDIA_ROOT
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -130,3 +132,7 @@ STATICFILES_DIR = (os.path.join(BASE_DIR, 'catalog/static'))
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
