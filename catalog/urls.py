14ff208d2dfbe8ec703d08be40bd5768405487d5
xziste <17alexdmz99@gmail.com>
2025-09-01 12:23:30 +0300

    update urls.py to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product_page
+from catalog.views import contacts, ProductListView, ProductDetailView
 
 app_name = CatalogConfig.name  # 'catalog'
 
 urlpatterns = [
-    path('home/', home, name='home'),
+    path('home/', ProductListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>/', product_page, name='product_page'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_page'),
 ]
