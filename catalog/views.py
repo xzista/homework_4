828f37ef2d78a56e9c461a92b9007e04cbe647b8
xziste <17alexdmz99@gmail.com>
2025-09-01 12:22:20 +0300

    update views.py to CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,22 +1,65 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 
 from catalog.models import Product, Contact
 
 
-def home(request):
-    all_products = Product.objects.all().order_by('-created_at')
-    latest_products = all_products[:5]
-    print("ПОСЛЕДНИЕ 5 ПРОДУКТОВ:")
-    for i, product in enumerate(latest_products, 1):
-        print(f'{i}. {product.name} категории: {product.category}, с ценой: {product.price} руб.')
+class ProductListView(ListView):
+    model = Product
+    template_name = 'home.html'
+    context_object_name = 'products'
 
-    context = {
-        'all_products': all_products,
-        'latest_products': latest_products
-    }
 
-    return render(request, 'home.html', context)
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name', 'price', 'category', 'description',]
+    template_name = 'product_create.html'
+    success_url = reverse_lazy('catalog:product_create.html')
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_page.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = self.get_object()
+
+        similar_products = Product.objects.filter(category=product.category).exclude(id=product.id).order_by('?')[:4]
+
+        context['similar_products'] = similar_products
+        return context
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'price', 'category', 'description',]
+    template_name = ''
+    success_url = reverse_lazy('catalog:')  #
+
+
+class ProductDeleteView(DetailView):
+    model = Product
+    template_name = 'product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:')  #
+
+
+# def home(request):
+#     all_products = Product.objects.all().order_by('-created_at')
+#     latest_products = all_products[:5]
+#     print("ПОСЛЕДНИЕ 5 ПРОДУКТОВ:")
+#     for i, product in enumerate(latest_products, 1):
+#         print(f'{i}. {product.name} категории: {product.category}, с ценой: {product.price} руб.')
+#
+#     context = {
+#         'all_products': all_products,
+#         'latest_products': latest_products
+#     }
+#
+#     return render(request, 'home.html', context)
 
 
 def contacts(request):
@@ -34,12 +77,12 @@ def contacts(request):
     return render(request, 'contacts.html', context)
 
 
-def product_page(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-
-    similar_products = Product.objects.filter(category=product.category).exclude(id=product.id).order_by('?')[:4]
-
-    context = {'product': product,
-               'similar_products': similar_products,
-               }
-    return render(request, 'product_page.html', context)
+# def product_page(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#
+#     similar_products = Product.objects.filter(category=product.category).exclude(id=product.id).order_by('?')[:4]
+#
+#     context = {'product': product,
+#                'similar_products': similar_products,
+#                }
+#     return render(request, 'product_page.html', context)
