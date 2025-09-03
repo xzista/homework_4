64a0ee08be80d905ab58a8c0959dc0d892be2667
xziste <17alexdmz99@gmail.com>
2025-09-03 08:47:18 +0300

    add BlogListView in views.py in app Blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from blog.models import BlogPost
+
+
+class BlogListView(ListView):
+    model = BlogPost
+    template_name = 'blog_main.html'
+    context_object_name = 'posts'
