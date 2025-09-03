e1a6b5d7a845d18794049dd2dee640a6137e012a
xziste <17alexdmz99@gmail.com>
2025-09-03 08:33:12 +0300

    add BlogPost in models.py in app Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок', help_text='Укажите заголовок')
+    content = models.TextField(verbose_name='Содержимое', help_text='Заполните содержимое')
+    image = models.ImageField(upload_to='blog/blogs_images', verbose_name='Изображение',
+                              help_text='Загрузите изображение', blank=True, null=True)
+    created_at = models.DateTimeField(verbose_name='Время создания', auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+    views_count = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
+        ordering = ['title', 'created_at', 'views_count', 'is_published', ]
+
+    def __str__(self):
+        return self.title
