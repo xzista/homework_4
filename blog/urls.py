736a9e8b9c00c97e91e4a51af43e086a778aaab2
xziste <17alexdmz99@gmail.com>
2025-09-03 10:44:03 +0300

    update urls.py in Blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogListView
+from blog.views import BlogListView, BlogDetailView
 
 app_name = BlogConfig.name  # 'blog'
 
 urlpatterns = [
     path('posts/', BlogListView.as_view(), name='posts'),
+    path('posts/<int:pk>/', BlogDetailView.as_view(), name='post_page'),
 ]
