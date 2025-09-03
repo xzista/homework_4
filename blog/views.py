3135777ce16697b2893e5de9434f6e0112126cd6
xziste <17alexdmz99@gmail.com>
2025-09-03 09:41:38 +0300

    add BlogDetailView in views.py in app Blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from blog.models import BlogPost
 
@@ -8,3 +8,12 @@ class BlogListView(ListView):
     model = BlogPost
     template_name = 'blog_main.html'
     context_object_name = 'posts'
+
+
+class BlogDetailView(DetailView):
+    model = BlogPost
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        return self.object
