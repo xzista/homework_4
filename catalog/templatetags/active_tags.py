963889b8a1ec3511aacd5222595001e1fba08e56
xziste <17alexdmz99@gmail.com>
2025-09-05 14:53:31 +0300

    update tag active and menu.html

--- catalog/templatetags/active_tags.py
+++ catalog/templatetags/active_tags.py
@@ -5,8 +5,11 @@ from django.conf import settings
 register = template.Library()
 
 @register.simple_tag
-def active(request, url_name):
-    return "active text-white bg-primary" if request.resolver_match.url_name == url_name else "text-white"
+def active(request, *url_names):
+    current_url_name = request.resolver_match.url_name
+    if current_url_name in url_names:
+        return "active text-white bg-primary"
+    return "text-white"
 
 
 @register.filter()
