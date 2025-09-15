40fce9a9ab8ecd9cef026b25759cc006ad19d6ff
xziste <17alexdmz99@gmail.com>
2025-09-15 17:05:53 +0300

    add field token for users

--- users/models.py
+++ users/models.py
@@ -7,6 +7,8 @@ class User(AbstractUser):
     country = models.CharField(max_length=35, verbose_name='Страна', blank=True, null=True, help_text='Укажите страну')
     avatar = models.ImageField(upload_to='users/avatars/', blank=True, null=True, help_text='Загрузите свой аватар')
 
+    token = models.CharField(max_length=100, verbose_name='Token', blank=True, null=True,)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
