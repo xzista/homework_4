8181baa3e3c3766f37544ef00659765be5b306d6
xziste <17alexdmz99@gmail.com>
2025-09-15 18:17:13 +0300

    update models.py for users

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    username = None
     email = models.EmailField(unique=True, verbose_name='Email')
     phone = models.CharField(max_length=35, verbose_name='Телефон', blank=True, null=True, help_text='Введите номер телефона')
     country = models.CharField(max_length=35, verbose_name='Страна', blank=True, null=True, help_text='Укажите страну')
