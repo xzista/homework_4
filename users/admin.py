7eb12d170d24c3b84616fed2b46ef557c00c36f4
xziste <17alexdmz99@gmail.com>
2025-09-15 18:14:12 +0300

    add users in admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "phone",
+        "country",
+        "avatar",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "country",
+        "is_active",
+    )
+    search_fields = ("email", "country", "phone",)
