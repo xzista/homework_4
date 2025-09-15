646c67943edf5fdcde16d5c3f00e768ee81a00a6
xziste <17alexdmz99@gmail.com>
2025-09-15 18:17:41 +0300

    update urls.py in users

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+from users.views import RegisterView, email_verification
 
 app_name = 'users'
 
 urlpatterns = [
-    path("login/", LoginView.as_view(template_name='login.html'), name="home"),
+    path("login/", LoginView.as_view(template_name='login.html'), name="login"),
+    path("logout/", LogoutView.as_view(next_page='catalog:home'), name="logout"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("email-confirm/<str:token>/", email_verification, name="email-confirm"),
 ]
