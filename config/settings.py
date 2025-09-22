d9e6fd58681b2f7a7874549e85f762e230421f36
xziste <17alexdmz99@gmail.com>
2025-09-22 23:06:26 +0300

    fixed bug

--- config/settings.py
+++ config/settings.py
@@ -159,4 +159,4 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 CACHE_ENABLED = True
 if CACHE_ENABLED:
-    CACHES = {'default': {'BACKEND': 'django.cache.backends.redis.RedisCache', 'LOCATION': 'redis://127.0.0.1:6379'}}
+    CACHES = {'default': {'BACKEND': 'django.core.cache.backends.redis.RedisCache', 'LOCATION': 'redis://127.0.0.1:6379'}}
