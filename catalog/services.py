9e0bb62bbb1fdbcb11ad1a5206b1fdc19142d05e
xziste <17alexdmz99@gmail.com>
2025-09-23 17:45:58 +0300

    add get_products_from_cache in services.py

---
+++ catalog/services.py
@@ -0,0 +1,17 @@
+from django.core.cache import cache
+
+from catalog.models import Product
+from config.settings import CACHE_ENABLED
+
+
+def get_products_from_cache():
+    """Функция для получения списка продуктов из кэша, или кэширование данный при его отсутствии"""
+    if not CACHE_ENABLED:
+        return Product.objects.all()
+    key = 'product_list'
+    products = cache.get(key)
+    if products is not None:
+        return products
+    products = Product.objects.all()
+    cache.set(key, products)
+    return products
